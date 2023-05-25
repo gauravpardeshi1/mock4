ed5f621bfad546f96feeabbc55aba1895b1f39c4
Ga1156 <gauravpardeshi068@gmail.com>
2023-05-25 15:37:16 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonserver=require('json-server');
 const server=jsonserver.create();
 const route=jsonserver.route('db.json');
 const middlewares=jsonserver.defaults();
-const port= process.env.PORT || 8080;
+//const port= process.env.PORT || 8080;
 server.use(middlewares)
 server.use(route) 
-server.listen(port);
+server.listen(8080);
